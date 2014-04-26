0f64442cf09e14a0921231c4b2995f8cc8a2e5d8
Joe Burgess <joe@flatironschool.com>
2014-04-26 17:20:03 -0400

    Adding My Solution

--- multiThreadLab/multiThreadLab/FISViewController.m
+++ multiThreadLab/multiThreadLab/FISViewController.m
@@ -15,7 +15,9 @@
 @property (weak, nonatomic) IBOutlet UILabel *stateLabel;
 @property (weak, nonatomic) IBOutlet UILabel *latitudeLabel;
 @property (weak, nonatomic) IBOutlet UILabel *longitudeLabel;
+
 - (IBAction)searchZipCodeTapped:(id)sender;
+- (void)setRandomBGColor;
 @end
 
 @implementation FISViewController
@@ -33,6 +35,7 @@
 {
     [super viewDidLoad];
     self.view.accessibilityLabel=@"Main View";
+    [NSTimer scheduledTimerWithTimeInterval:.25 target:self selector:@selector(setRandomBGColor) userInfo:nil repeats:YES];
     // Do any additional setup after loading the view.
 }
 
@@ -42,15 +45,60 @@
     // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+-(void)searchZipCodeTapped:(id)sender
 {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
+    if ([self.zipCode.text length]!=5)
+    {
+        UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"Zip Code Error" message:@"Zip Codes need to be 5 digits" delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
+        [alertView show];
+        return;
+    }
+    NSOperationQueue *queue = [[NSOperationQueue alloc] init];
+    [queue addOperationWithBlock:^{
+        NSString *filePath = [[NSBundle mainBundle] pathForResource:@"zip_codes_states" ofType:@"csv"];
+        NSString *contents = [NSString stringWithContentsOfURL:[NSURL fileURLWithPath:filePath] encoding:NSUTF8StringEncoding error:nil];
+        NSArray *lines = [contents componentsSeparatedByString:@"\n"];
+        BOOL found=NO;
+        for (NSString *line in lines) {
+            NSArray *items = [line componentsSeparatedByString:@","];
+            if ([items count]==6 && ![items[0] isEqualToString:@"\"zip_code\""] ) {
+                NSString *zip = [items[0] stringByReplacingOccurrencesOfString:@"\"" withString:@""];
+                NSString *latitude = [items[1] stringByReplacingOccurrencesOfString:@"\"" withString:@""];
+                NSString *longitude = [items[2] stringByReplacingOccurrencesOfString:@"\"" withString:@""];
+                NSString *city = [items[3] stringByReplacingOccurrencesOfString:@"\"" withString:@""];
+                NSString *state = [items[4] stringByReplacingOccurrencesOfString:@"\"" withString:@""];
+                NSString *county = [items[5] stringByReplacingOccurrencesOfString:@"\"" withString:@""];
 
+                if ([zip isEqualToString:self.zipCode.text]) {
+                    [[NSOperationQueue mainQueue] addOperationWithBlock:^{
+                        self.cityLabel.text=city;
+                        self.countyLabel.text=county;
+                        self.latitudeLabel.text=latitude;
+                        self.longitudeLabel.text=longitude;
+                        self.stateLabel.text=state;
+                    }];
+                    found=YES;
+                    return;
+                }
+            }
+        }
+        if (!found)
+        {
+            [[NSOperationQueue mainQueue] addOperationWithBlock:^{
+                UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"Zip Code Error" message:@"Couldn't find that zip code" delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
+                [alertView show];
+            }];
+            return;
+        }
+    }];
+}
+
+- (void)setRandomBGColor
+{
+    CGFloat hue = ( arc4random() % 256 / 256.0 );  //  0.0 to 1.0
+    CGFloat saturation = ( arc4random() % 128 / 256.0 ) + 0.5;  //  0.5 to 1.0, away from white
+    CGFloat brightness = ( arc4random() % 128 / 256.0 ) + 0.5;  //  0.5 to 1.0, away from black
+    UIColor *color = [UIColor colorWithHue:hue saturation:saturation brightness:brightness alpha:1];
+    self.view.backgroundColor = color;
+}
 @end
